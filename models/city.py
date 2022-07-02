0d5f2badd0e9e2590a79cb2e2c89d2df25e98371
abdou50 <abdelkaderraddadi988@gmail.com>
2022-07-02 13:05:04 +0300

    l

--- models/city.py
+++ models/city.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 """
-Place module
+City module
 """
 from models.base_model import BaseModel
 
 
-class Place(BaseModel):
+class City(BaseModel):
     """
-    Place class
+    City class
     inhereted from BaseModel
     """
-    city_id = ""
-    user_id = ""
+    state_id = ""
     name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
